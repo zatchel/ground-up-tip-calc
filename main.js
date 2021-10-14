735eaf6909d68637320eec3dc36fc95e36508633
zatchel <83443465+zatchel@users.noreply.github.com>
2021-10-13 20:44:06 -0500

    init commit, layout done starting on logic.

---
+++ main.js
@@ -0,0 +1,30 @@
+// const billAmount = document.querySelector(["data-billAmount"]);
+const tipFive = document.querySelector(["data-tipFive"]);
+const tipTen = document.querySelector(["data-tipTen "]);
+const tipFifteen = document.querySelector(["tipFifteen"]);
+const tipTwenty  = document.querySelector(["tipTwenty "]);
+const tipCustom = document.querySelector(["data-tipCustom"]);
+const numberPeople = document.querySelector(["data-numberPeople"]);
+const calculateButton = document.getElementById("calculate");
+const tipAmountDisplay = document.querySelector(["data-tipAmountDisplay"]);
+const totalDisplay = document.querySelector(["data-totalDisplay"]);
+const selectDrop = document.getElementById("selectDrop");
+
+
+calculateButton.addEventListener('click', function(){
+    let billAmount = document.getElementById('billAmount').value;
+    let peopleNumber = document.getElementById('numberPeople').value;
+    
+    
+    console.log(billAmount);
+    console.log(peopleNumber);
+})
+
+function becomeActive(){
+    let boxExists = document.getElementsByClassName("newCustomText").length;
+    if(boxExists === 0){
+        document.getElementById("custom").outerHTML = "<input type='text' class='newCustomText'> <br> <span class='placeholder'>%</span>";
+
+    console.log("custom");
+    }
+}
